0f543b11d020841c30c8198fd2fe7f695d9698f5
Yaroslav Serhieiev <yaroslavs@wix.com>
2022-10-10 11:13:26 +0300

    fix download script for win32

--- scripts/utils/gsheets.mjs
+++ scripts/utils/gsheets.mjs
@@ -1,8 +1,25 @@
 import 'zx/globals';
+import https from 'https';
+import fs from 'fs';
 
 export async function downloadSheet(outFile, baseUrl, gid) {
   const url = `${baseUrl}?output=csv&format=csv&single=true&gid=${gid}`;
-  await $`curl -L -o ${outFile} ${url}`;
+  console.log('Downloading: ' + chalk.green(outFile));
+
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(outFile);
+    const request = https.get(url, function(response) {
+      response.pipe(file);
+
+      file.on("error", reject);
+      file.on("finish", () => {
+        file.close();
+        resolve();
+      });
+    });
+
+    request.on('error', reject);
+  });
 }
 
 export async function redownloadSheet(outFile, baseUrl, gid) {
